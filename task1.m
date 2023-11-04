5cc45c9a2f28c0b54d67bb4a682fb2b4e80f2501
Aly Ghallab <alyghallab@icloud.com>
2023-11-04 06:18:12 -0400

    Roi Updated Completed Tasks

--- task1.m
+++ task1.m
@@ -1,42 +1,63 @@
-load 'Parameters_V1_1.mat'
-load 'Parameters_V2_1.mat'
 load 'mocapPoints3D.mat'
+load 'Parameters_V1_1.mat'
 
 % get the matrix of pixel coordinates converted from world coordinates
-%currently not properly creating a matrix of pixel coordinates
-Pmat = Parameters.Pmat;
-Kmat = Parameters.Kmat;
-Rmat = Parameters.Rmat;
-t = Pmat(1:3, 4);,
+Pmat_im1 = Parameters.Pmat;
+Kmat_im1 = Parameters.Kmat;
+Rmat_im1 = Parameters.Rmat;
+t_im1 = Pmat_im1(1:3, 4);
 
-Pmat_padded = [Pmat; zeros(1,3) ones(1,1)];
-
-zero_col = zeros(size(Kmat,1),1);
-Kmat_adjusted = [Kmat, zero_col];
-
+% created point world coordinate with Homogeneous coordinate
 pts3D_worldpoint = [pts3D; ones(1,39)];
 
-
+% Implementing Pinpoint Camera Model for all 39 film points
+Im1_film_matrix = [];
 for i = 1:size(pts3D,2)
-    disp(i);
-    pixel_coords_math = Kmat * Pmat * pts3D_worldpoint(:,i);
-    resultIm1 = pixel_coords_math(:)/pixel_coords_math(3,1);
+    pixel_coords_math = Kmat_im1 * Pmat_im1 * pts3D_worldpoint(:,i);
+    Im1_filmcoord = [pixel_coords_math(1,1)/pixel_coords_math(3,1); pixel_coords_math(2,1)/pixel_coords_math(3,1);1];
+    Im1_film_matrix = [Im1_film_matrix Im1_filmcoord];
 end
 
-for i = 1:size(pts3D,2)
-    disp(i);
-    pixel_coords_math = Kmat * Pmat * pts3D_worldpoint(:,i);
-    resultIm2 = pixel_coords_math(:)/pixel_coords_math(3,1);
-end
-
+% Showing film points with respect to body in image
 im1corrected = imread("im1corrected.jpg");
-im2corrected = imread("im2corrected.jpg");
-
-show_im1 = imshow(im1corrected);
-show_im2 = imshow(im2corrected);
-
+figure; show_im1 = imshow(im1corrected);
 hold on;
-for j = 1:size(pts3D,2)
-    scatter(pixel_coords(1,j),pixel_coords(2,j));
+x1 = Im1_film_matrix(1,:);
+y1 = Im1_film_matrix(2,:);
+scatter(x1,y1);
+hold off;
+
+load 'Parameters_V2_1.mat'
+
+% get the matrix of pixel coordinates converted from world coordinates
+Pmat_im2 = Parameters.Pmat;
+Kmat_im2 = Parameters.Kmat;
+Rmat_im2 = Parameters.Rmat;
+t_im2 = Pmat_im2(1:3, 4);
+
+% created Pmat with homogeneous coordinates
+Pmat_padded = [Pmat_im2; zeros(1,3) ones(1,1)];
+
+% Created Calibrated Matrix
+zero_col = zeros(size(Kmat_im2,1),1);
+Kmat_adjusted = [Kmat_im2, zero_col];
+
+% created point world coordinates with Homogeneous coordinate
+pts3D_worldpoint = [pts3D; ones(1,39)];
+
+% Implementing Pinpoint Camera Model for all 39 film points
+Im2_film_matrix = [];
+for i = 1:size(pts3D,2)
+    pixel_coords_math2 = Kmat_im2 * Pmat_im2 * pts3D_worldpoint(:,i);
+    Im2_filmcoord = [pixel_coords_math2(1,1)/pixel_coords_math2(3,1); pixel_coords_math2(2,1)/pixel_coords_math2(3,1);1];
+    Im2_film_matrix = [Im2_film_matrix Im2_filmcoord];
 end
+
+% Showing film points with respect to body in image
+im2corrected = imread("im2corrected.jpg");
+figure; show_im2 = imshow(im2corrected);
+hold on;
+x2 = Im2_film_matrix(1,:);
+y2 = Im2_film_matrix(2,:);
+scatter(x2,y2);
 hold off;
---
+++ task2.m
@@ -0,0 +1,63 @@
+%Run Task 1 first to get the necessary variables
+
+R_im1_transpose = transpose(Rmat_im1);
+R_im2_transpose = transpose(Rmat_im2);
+
+% finding baseline from camera 1 (right) and camera 2 (left)
+O_Im1 = -R_im1_transpose * t_im1;
+O_Im2 = -R_im2_transpose * t_im2;
+T = O_Im1 - O_Im2;
+
+% Defining observed matrix
+P_w_matrix = ones(4, 39);
+
+% Initiating Triangulation Process
+for i = 1:size(pts3D,2)
+
+    % Grabbing results from task 1 to create viewing rays 
+    im1_FilmPoint = Im1_film_matrix(:,i);
+    u_im1 = R_im1_transpose * ((Kmat_im1)\im1_FilmPoint);
+
+    im2_FilmPoint = Im2_film_matrix(:,i);
+    u_im2 = R_im2_transpose * ((Kmat_im2)\im2_FilmPoint);
+
+    % defining viewing rays u1 and u2
+    norm_u_im1 = norm(u_im1);
+    norm_u_im2 = norm(u_im2);
+    u1 = u_im1 / norm_u_im1;
+    u2 = u_im2 / norm_u_im2;
+
+    % defining non intersecting viewing ray u3 based on u1 and u2
+    u3_A = cross(u_im1, u_im2);
+    u3_B = norm(cross(u_im1, u_im2));
+    u3 = u3_A/u3_B;
+
+    % creating matrix of viewing rays
+    A = [u2, -u1, u3];
+    
+    % defining scaling coefficients that go along with the viewing rays and
+    % separated into three coefficients for each ray
+    coeffs = A\T;
+
+    a = coeffs(1,1);
+    b = coeffs(2,1);
+    c = coeffs(3,1);
+
+    % creating world point coordinates from each camera 
+    P_im1 = O_Im1 + b*u1;
+    P_im2 = O_Im2 + a*u2;
+
+    % Combining world coordinates from each camera to define the observed
+    % world coordinates then put them into a matrix of observed point 
+    % world coordinates.
+    P_w = (P_im1 + P_im2)/2;
+    P_w_matrix(1:3, i) = P_w;
+end
+
+% Applying Mean Squared Error Equation to find difference betweeen 
+% observed and given coordinates.
+World_coord_diff = P_w_matrix - pts3D_worldpoint;
+World_coord_diff_squared = World_coord_diff.^2;
+n = 2*size(pts3D);
+World_sum = sum(World_coord_diff_squared, 'all');
+MeanSquaredError = World_sum./n 
