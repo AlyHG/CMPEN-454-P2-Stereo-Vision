868df187cd2d99bc8ff758003722123fbe36bacc
Aly Ghallab <alyghallab@icloud.com>
2023-11-05 18:49:54 -0500

    Aly - Task 4 Incomplete (No CVT)

--- task4.m
+++ task4.m
@@ -1,96 +1,157 @@
-% Load the image correspondences
-load 'Task3_pixel_coords.mat'
 
-% Read the images
-img1 = imread('im1corrected.jpg');
-img2 = imread('im2corrected.jpg');
+%test eight point algorithm
 
-% Using 8 points of correspondence (3 for Floor, 3 for Wall, 2 for Door)
-points_img1 = [floor_1; Door_1; Wall_1];
-points_img2 = [floor_2; Door_2; Wall_2];
+im = imread('im1corrected.jpg');
+im2 = imread('im2corrected.jpg');
 
-% % Estimate the Fundamental Matrix
-% [F, inliers] = estimateFundamentalMatrix(points_img1, points_img2, 'Method', 'Norm8Point');
+% Demo Algorithm By Professor
+figure(1); imagesc(im); axis image; drawnow;
+figure(2); imagesc(im2); axis image; drawnow;
 
 
-% --- Begin: Eight-Point Algorithm for Fundamental Matrix ---
+figure(1); [x1,y1] = getpts;
+figure(1); imagesc(im); axis image; hold on
+for i=1:length(x1)
+   h=plot(x1(i),y1(i),'*'); set(h,'Color','g','LineWidth',2);
+   text(x1(i),y1(i),sprintf('%d',i));
+end
+hold off
+drawnow;
 
-% Extract the x and y coordinates from the image points
-x1 = points_img1(:, 1);
-y1 = points_img1(:, 2);
-x2 = points_img2(:, 1);
-y2 = points_img2(:, 2);
 
-% Hartley Preconditioning
-% Calculate the mean and standard deviation for the points in the first image
+figure(2); imagesc(im2); axis image; drawnow;
+[x2,y2] = getpts;
+figure(2); imagesc(im2); axis image; hold on
+for i=1:length(x2)
+   h=plot(x2(i),y2(i),'*'); set(h,'Color','g','LineWidth',2);
+   text(x2(i),y2(i),sprintf('%d',i));
+end
+hold off
+drawnow;
+
+% % Using points of correspondence from Task 3
+% points_img1 = [floor_1; Door_1; Wall_1];
+% points_img2 = [floor_2; Door_2; Wall_2];
+% 
+% % Extract x and y coordinates for the two images
+% x1 = points_img1(:, 1);
+% y1 = points_img1(:, 2);
+% x2 = points_img2(:, 1);
+% y2 = points_img2(:, 2);
+% 
+% % Visualize the selected points on the first image
+% figure(1); imagesc(im); axis image; hold on;
+% for i=1:length(x1)
+%    h = plot(x1(i), y1(i), '*');
+%    set(h, 'Color', 'g', 'LineWidth', 2);
+%    text(x1(i), y1(i), sprintf('%d', i));
+% end
+% hold off;
+% 
+% % Visualize the selected points on the second image
+% figure(2); imagesc(im2); axis image; hold on;
+% for i=1:length(x2)
+%    h = plot(x2(i), y2(i), '*');
+%    set(h, 'Color', 'g', 'LineWidth', 2);
+%    text(x2(i), y2(i), sprintf('%d', i));
+% end
+% hold off;
+
+
+%do Hartley preconditioning
+savx1 = x1; savy1 = y1; savx2 = x2; savy2 = y2;
 mux = mean(x1);
 muy = mean(y1);
-stdxy = (std(x1) + std(y1)) / 2;
-
-% Define a transformation matrix T1 based on the mean and standard deviation
-T1 = [1 0 -mux; 0 1 -muy; 0 0 stdxy] / stdxy;
-
-% Normalize the points in the first image
-nx1 = (x1 - mux) / stdxy;
-ny1 = (y1 - muy) / stdxy;
-
-% Repeat the above normalization process for the points in the second image
+stdxy = (std(x1)+std(y1))/2;
+T1 = [1 0 -mux; 0 1 -muy; 0 0 stdxy]/stdxy;
+nx1 = (x1-mux)/stdxy;
+ny1 = (y1-muy)/stdxy;
 mux = mean(x2);
 muy = mean(y2);
-stdxy = (std(x2) + std(y2)) / 2;
-T2 = [1 0 -mux; 0 1 -muy; 0 0 stdxy] / stdxy;
-nx2 = (x2 - mux) / stdxy;
-ny2 = (y2 - muy) / stdxy;
+stdxy = (std(x2)+std(y2))/2;
+T2 = [1 0 -mux; 0 1 -muy; 0 0 stdxy]/stdxy;
+nx2 = (x2-mux)/stdxy;
+ny2 = (y2-muy)/stdxy;
 
-% Construct the matrix A, based on the normalized points
 A = [];
-for i=1:length(nx1)
+for i=1:length(nx1);
     A(i,:) = [nx1(i)*nx2(i) nx1(i)*ny2(i) nx1(i) ny1(i)*nx2(i) ny1(i)*ny2(i) ny1(i) nx2(i) ny2(i) 1];
 end
-
-% Calculate the Fundamental Matrix F from A using the smallest eigenvalue
+%get eigenvector associated with smallest eigenvalue of A' * A
 [u,d] = eigs(A' * A,1,'SM');
 F = reshape(u,3,3);
 
-% Enforce a rank-2 constraint on F. This is because the Fundamental matrix is inherently rank-2
+%make F rank 2
+oldF = F;
 [U,D,V] = svd(F);
 D(3,3) = 0;
 F = U * D * V';
 
-% Undo the normalization to obtain the actual Fundamental matrix
+%unnormalize F to undo the effects of Hartley preconditioning
 F = T2' * F * T1;
-% --- End:  Eight-Point Algorithm for Fundamental Matrix ---
 
-% Display the epipolar lines in the first image
-figure, imshow(img1);
-title('Epipolar Lines in First Image');
-hold on;
-epiLines1 = epipolarLine(F', points_img2);
-points = lineToBorderPoints(epiLines1, size(img1));
-line(points(:, [1,3])', points(:, [2,4])', 'Color', 'g', 'LineWidth', 1.5);
-hold off;
-
-% Display the equations of the epipolar lines for the first image
-disp('Epipolar Lines for First Image:');
-for i = 1:size(epiLines1, 1)
-    fprintf('Line %d: %fx + %fy + %f = 0\n', i, epiLines1(i,1), epiLines1(i,2), epiLines1(i,3));
+colors =  'bgrcmykbgrcmykbgrcmykbgrcmykbgrcmykbgrcmykbgrcmyk';
+%overlay epipolar lines on im2
+L = F * [x1' ; y1'; ones(size(x1'))];
+[nr,nc,nb] = size(im2);
+figure(2); clf; imagesc(im2); axis image;
+hold on; plot(x2,y2,'*'); hold off
+for i=1:length(L)
+    a = L(1,i); b = L(2,i); c=L(3,i);
+    if (abs(a) > (abs(b)))
+       ylo=0; yhi=nr; 
+       xlo = (-b * ylo - c) / a;
+       xhi = (-b * yhi - c) / a;
+       hold on
+       h=plot([xlo; xhi],[ylo; yhi]);
+       set(h,'Color',colors(i),'LineWidth',2);
+       hold off
+       drawnow;
+    else
+       xlo=0; xhi=nc; 
+       ylo = (-a * xlo - c) / b;
+       yhi = (-a * xhi - c) / b;
+       hold on
+       h=plot([xlo; xhi],[ylo; yhi],'b');
+       set(h,'Color',colors(i),'LineWidth',2);
+       hold off
+       drawnow;
+    end
 end
 
-% Display the epipolar lines in the second image
-figure, imshow(img2);
-title('Epipolar Lines in Second Image');
-hold on;
-epiLines2 = epipolarLine(F, points_img1);
-points = lineToBorderPoints(epiLines2, size(img2));
-line(points(:, [1,3])', points(:, [2,4])', 'Color', 'g', 'LineWidth', 1.5);
-hold off;
 
-% Display the equations of the epipolar lines for the second image
-disp('Epipolar Lines for Second Image:');
-for i = 1:size(epiLines2, 1)
-    fprintf('Line %d: %fx + %fy + %f = 0\n', i, epiLines2(i,1), epiLines2(i,2), epiLines2(i,3));
+%overlay epipolar lines on im1
+L = ([x2' ; y2'; ones(size(x2'))]' * F)' ;
+[nr,nc,nb] = size(im);
+figure(1); clf; imagesc(im); axis image;
+hold on; plot(x1,y1,'*'); hold off
+for i=1:length(L)
+    a = L(1,i); b = L(2,i); c=L(3,i);
+    if (abs(a) > (abs(b)))
+       ylo=0; yhi=nr; 
+       xlo = (-b * ylo - c) / a;
+       xhi = (-b * yhi - c) / a;
+       hold on
+       h=plot([xlo; xhi],[ylo; yhi],'b');
+       set(h,'Color',colors(i),'LineWidth',2);
+       hold off
+       drawnow;
+    else
+       xlo=0; xhi=nc; 
+       ylo = (-a * xlo - c) / b;
+       yhi = (-a * xhi - c) / b;
+       hold on
+       h=plot([xlo; xhi],[ylo; yhi],'b');
+       set(h,'Color',colors(i),'LineWidth',2);
+       hold off
+       drawnow;
+    end
 end
 
-% Display the Fundamental Matrix
-disp('Fundamental Matrix:');
-disp(F);
+disp('Fundamental Matrix * 1000')
+for j=1:3
+    for i=1:3
+        fprintf('%10g ',10000*F(j,i));
+    end
+    fprintf('\n');
+end
